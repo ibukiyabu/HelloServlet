4e1534b92932ccaa3e91f8221055cf57005f6e6c
ibukiyabu <ibuki.y2115@gmail.com>
2024-02-03 23:29:01 +0900

    HelloServlet.java追記

--- src/main/java/HelloServlet.java
+++ src/main/java/HelloServlet.java
@@ -1,6 +1,8 @@
 
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -27,15 +29,15 @@ public class HelloServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		PrintWriter out = response.getWriter();
+		out.println("<HTML>");
+		out.println("<BODY>");
+		out.println("<H3>Hello Servlet!</H3>");
+		out.println("</BODY>");
+		out.println("</HTML>");
+		
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
 
 }
